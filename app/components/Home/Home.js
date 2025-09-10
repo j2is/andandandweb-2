7f78509f05a67cbb3f7134fa81358153f7b52623
j2is <studio@jaisand.hu>
2025-09-10 16:48:33 +0100

    updated

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomeStyles from "./Home.styled";
 import FloatingParallaxScroll from '../FloatingParallaxScroll';
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
 	return (
@@ -12,6 +13,9 @@ export default function Home() {
 			<HomeStyles>
 				<main>
 					<FloatingParallaxScroll />
+					<section className="logo">
+						<Image src="/images/logo.svg" alt="And And And Studio" width={375} height={29} />
+					</section>
 				</main>
 			</HomeStyles>
 		</>
--- app/components/Home/Home.styled.js
+++ app/components/Home/Home.styled.js
@@ -7,6 +7,18 @@ const HomeStyles = styled.section`
   width: 100%;
 	height: 100%;
 	min-height: 100dvh;
+
+	.logo {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100dvh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		z-index: 1;
+	}
 `;
 
 export default HomeStyles;
